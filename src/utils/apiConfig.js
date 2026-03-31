850af351642d149e7038578f3aae8b5da68a75c7
midlaj <midhumidlaj776@gmail.com>
2026-03-31 20:03:32 +0530

    orgin  changed

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,6 +1,6 @@
 // src/utils/apiConfig.js
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL = "https://sovereigns.site/api";
+  import.meta.env.VITE_API_BASE_URL = "https://sovereigns.site";
 
 export default API_BASE_URL;
